4458dfb21235037647d9268d0b9488c8e164c592
cgusti <89992144+cgusti@users.noreply.github.com>
2023-02-22 15:03:31 -0800

    Delete Reflect-Journaling-Web-Application directory

--- Reflect-Journaling-Web-Application/src/components/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders the app component with the word Blog on it", async () => {
-  render(<App />);
-  await screen.findByText(/Blog/);
-});
